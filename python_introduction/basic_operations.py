373bffca705bd2d0ebe912b15f15c327b1069630
Henry Nyarko <henryeknyarko@gmail.com>
2025-05-28 15:51:33 +0000

    Fix: corrected variable assignments in basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,6 +1,7 @@
-a = 10
-b = 5
 
-print("Addition of", a, "and", b, "is", a + b)
-print("Subtraction of", a, "and", b, "is", a - b)
-print("Multiplication of", a, "and", b, "is", a * b)
+Number1 = 10
+Number2 = 5
+
+print("Addition of",Number1, "and", Number2, "is", Number1 + Number2)
+print("Subtraction of", Number1, "and", Number2, "is", Number1 - Number2)
+print("Multiplication of", Number1, "and", Number2, "is", Number1 * Number2)
