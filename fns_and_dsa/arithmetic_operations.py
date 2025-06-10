9e453d709fae58296f4a82020c9a45ce35e53519
Henry Nyarko <henryeknyarko@gmail.com>
2025-06-10 22:40:24 +0000

    Fix: Remove duplicate functions and align with ALX requirements

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,6 @@
- num1 + num2
+def perform_operation(num1: float, num2: float, operation: str):
+    if operation == 'add':
+        return num1 + num2
     elif operation == 'subtract':
         return num1 - num2
     elif operation == 'multiply':
@@ -8,16 +10,8 @@
             return None  
         return num1 / num2
     else:
-        return None def perform_operation(num1: float, num2: float, operation: str):
-    if operation == 'add':
-        return num1 + num2
-    elif operation == 'subtract':
-        return num1 - num2
-    elif operation == 'multiply':
-        return num1 * num2
-    elif operation == 'divide':
-        if num2 == 0:
-            return "Error: Division by zero"
-        return num1 / num2
-    else:
-        return "Error: Invalid operation"
+        return None 
+    
+    
+  
+        
