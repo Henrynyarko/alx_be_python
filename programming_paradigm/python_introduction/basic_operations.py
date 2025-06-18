4ee89aaed3aa3c8b6925f4ae76eb5cf38b69fa16
Henry Nyarko <henryeknyarko@gmail.com>
2025-06-18 13:53:51 +0000

    Add SimpleCalculator and full test suite

---
+++ programming_paradigm/python_introduction/basic_operations.py
@@ -0,0 +1,7 @@
+
+Number1 = 10
+Number2 = 5
+
+print("Addition of",Number1, "and", Number2, "is", Number1 + Number2)
+print("Subtraction of", Number1, "and", Number2, "is", Number1 - Number2)
+print("Multiplication of", Number1, "and", Number2, "is", Number1 * Number2)
